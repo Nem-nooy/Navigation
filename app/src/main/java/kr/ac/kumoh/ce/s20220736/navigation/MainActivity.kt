1a030fea328aae5ddf353919ce5589eea78ac322
Nem-nooy <sgu101310@gmail.com>
2024-12-16 11:38:14 +0900

    🌐 Drawer 화면 이동

--- app/src/main/java/kr/ac/kumoh/ce/s20220736/navigation/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20220736/navigation/MainActivity.kt
@@ -8,16 +8,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHost
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.launch
 import kr.ac.kumoh.ce.s20220736.navigation.ui.theme.NavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,21 +40,73 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
     val navController = rememberNavController()
 
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        NavHost (
-            navController = navController,
-            startDestination = "nav_screen1",
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            composable("screen1") {
-                FirstScreen(navController)
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            DrawerSheet(drawerState) {
+                navController.navigate(it) {
+                    // 이미 화면이 스택에 있다면 새로 만들지 않고 기존 화면으로 이동
+                    launchSingleTop = true
+                    popUpTo(it) { inclusive = true }
+                }
             }
-            composable("screen2") {
-                SecondScreen(navController)
+        },
+        gesturesEnabled = true,
+    ) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+        ) { innerPadding ->
+            // 위로 이동
+            // val navController = rememberNavController()
+
+            NavHost(
+                navController = navController,
+                startDestination = "screen1",
+                modifier = Modifier.padding(innerPadding),
+            ) {
+                composable("screen1") {
+                    FirstScreen(navController)
+                }
+                composable("screen2") {
+                    SecondScreen(navController)
+                }
             }
         }
+    }
+}
+
+
+@Composable
+fun DrawerSheet(
+    drawerState: DrawerState,
+    onNavigate: (String) -> Unit,
+) {
+    val scope = rememberCoroutineScope()
+
+    ModalDrawerSheet {
+        NavigationDrawerItem(
+            label = { Text("화면 1") },
+            selected = false,
+            onClick = {
+                onNavigate("screen1")
+                scope.launch {
+                    drawerState.close()
+                }
+            }
+        )
+        NavigationDrawerItem(
+            label = { Text("화면 2") },
+            selected = false,
+            onClick = {
+                onNavigate("screen2")
+                scope.launch {
+                    drawerState.close()
+                }
+            }
+        )
     }
 }
 
@@ -61,7 +119,6 @@ fun FirstScreen(navController: NavController) {
                 navController.navigate("screen2") {
                     // 이미 화면이 스택에 있다면 새로 만들지 않고 기존 화면으로 이동
                     launchSingleTop = true
-                    popUpTo("screen2") { inclusive = true }
                 }
             }
         ) {
@@ -79,7 +136,6 @@ fun SecondScreen(navController: NavController) {
                 navController.navigate("screen1") {
                     // 이미 화면이 스택에 있다면 새로 만들지 않고 기존 화면으로 이동
                     launchSingleTop = true
-                    popUpTo("screen1") { inclusive = true }
                 }
             }
         ) {
